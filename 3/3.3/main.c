5faa9238f9cbd745b20038e839e01ddcfb639405
artsiomivanets <artem.ivanec96@gmail.com>
2022-05-09 17:49:04 +0300

    3.3

--- 3/3.3/main.c
+++ 3/3.3/main.c
@@ -8,31 +8,50 @@
 int state;
 
 
-void determine_state(char c, char next) {
-  if (OUTSIDE) {
-    if (c != '-') {
+void determine_state(char c) {
+  switch (state) {
+    case OUTSIDE:
+      state = c != '-' ? BEGIN : INSIDE;
+      return;
+    case BEGIN:
       state = INSIDE;
-    }
+      return;
+    case INSIDE:
+      state = c != '-' ? END : INSIDE;
+      return;
+    case END:
+      state = c != '-' ? BEGIN : OUTSIDE;
   }
-
 }
 
 void expand(char *s1, char *s2) {
-  int i, j;
-  char c;
+  int i, j, last;
+  char c, from, to;
   state = OUTSIDE;
+  last = 0;
 
-  for(i = 0; s1[i] != EOF; i++)
-    determine_state(s1[i], s1[i + 1]);
+  for(i = 0; s1[i] != '\0'; i++) {
+    c = s1[i];
+    determine_state(c);
+    switch (state) {
+      case BEGIN:
+        from = c;
+        break;
+      case END:
+        to = c;
+        for(j = from; j <= to; j++) {
+          s2[last] = j;
+          last++;
+        }
+    }
+  }
 }
 
 int main() {
   int c;
   char s[50];
 
-
-  expand("a-c3-6e-f-i", s);
-
+  expand("a-c3-6e-i", s);
 
   printf("%s", s);
 }
